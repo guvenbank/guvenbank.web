9e7fc4647ad6d7697981c592d79ed5bd8c8482e1
eecelik <enescelik.41@gmail.com>
2019-10-12 14:19:33 +0300

    edit v1

--- Web/Web/Controllers/AccountController.cs
+++ Web/Web/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -22,8 +23,13 @@ namespace Web.Controllers
 
         public IActionResult Login()
         {
+            //LoginModel loginModel = new LoginModel();
+            //loginModel.TC = "54862355952";
+            //loginModel.Password = "123456";
 
+           
             return View();
+
         }
 
         public IActionResult Register()
@@ -32,38 +38,44 @@ namespace Web.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //public IActionResult Login(LoginModel loginModel)
+        //{
+
+        //    HttpClient httpClient = new HttpClient();
+        //    httpClient.BaseAddress = new Uri("http://192.168.1.34:5002/api/auth/login");
+
+        //    string jsonData = JsonConvert.SerializeObject(loginModel);
+
+        //    var content = new StringContent(jsonData.ToString(), Encoding.UTF8, "application/json");
+        //    HttpResponseMessage response = httpClient.PostAsync("/api/auth/login", content).Result;
+
+        //    string responseBody = response.Content.ReadAsStringAsync().Result;
+
+        //    JObject responseJson = JsonConvert.DeserializeObject(responseBody) as JObject;
+
+
+        //    if ((responseJson["status"].ToString() == "success") && (Convert.ToInt32(response.StatusCode) == 200))
+        //    {
+        //        //HttpContext.Response.Cookies.Append("token", responseJson["token"].ToString());
+        //        return RedirectToAction("Index", "Account");
+                
+        //    }
+
+        //    else if ((responseJson["status"].ToString() == "failed") && (Convert.ToInt32(response.StatusCode) == 200))
+        //    {
+        //        return RedirectToAction("Login","Account");
+        //        //databag ile şifre yada tc yanlış hatası döndür.
+        //    }
+
+        //    else
+        //    {
+        //        // login ekranına dön hata oluştu yazısı yazdır.
+        //    }
+        //    return View();
+        //}
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Login(LoginModel loginModel)
-        {
-
-            HttpClient httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("http://192.168.1.34:5002/api/auth/login");
-
-            string jsonData = JsonConvert.SerializeObject(loginModel);
-
-            var content = new StringContent(jsonData.ToString(), Encoding.UTF8, "application/json");
-            HttpResponseMessage response = httpClient.PostAsync("/api/auth/login", content).Result;
-
-            string responseBody = response.Content.ReadAsStringAsync().Result;
-
-            JObject responseJson = JsonConvert.DeserializeObject(responseBody) as JObject;
-
-
-            if (responseJson["status"].ToString() == "success")
-            {
-                //Index sayfasına gonder
-            }
-
-            else
-            {
-                // uyarı verdir.
-            }
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
         public IActionResult Register(RegisterModel registerModel)
         {
 
@@ -80,17 +92,33 @@ namespace Web.Controllers
             JObject responseJson = JsonConvert.DeserializeObject(responseBody) as JObject;
 
 
-            if (responseJson["status"].ToString() == "success")
+            if ((responseJson["status"].ToString() == "success") && (Convert.ToInt32(response.StatusCode) == 200))
             {
-                //Index sayfasına gonder
+                return RedirectToAction("Index", "Account");
+            }
+
+            else if ((responseJson["status"].ToString() == "failed") && (Convert.ToInt32(response.StatusCode) == 200))
+            {
+                return RedirectToAction("Register", "Account");
+                //databag ile hatayı döndür register sayfasına ve sayfada göster
             }
 
             else
             {
-                // uyarı verdir.
+                // register ekranına dön hata oluştu yazısı yazdır.
             }
-
             return View();
         }
+
+        //public IActionResult Accounts()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public IActionResult Accounts()
+        //{
+        //    return View();
+        //}
     }
 }
