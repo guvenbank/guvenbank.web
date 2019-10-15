4e3987c7a58df11eb335d519018ccbf9240acc68
eecelik <enescelik.41@gmail.com>
2019-10-15 18:10:16 +0300

    Transfer işlemleri başlangıcı

--- Web/Web/Controllers/BankAccountController.cs
+++ Web/Web/Controllers/BankAccountController.cs
@@ -39,7 +39,7 @@ namespace Web.Controllers
             {
                 //hata döndür
             }
-
+    
             else
             {
                 // hata döndür
@@ -64,19 +64,61 @@ namespace Web.Controllers
 
             if ((responseJson["status"].ToString() == "success") && (Convert.ToInt32(response.StatusCode) == 200))
             {
-                //Hesap Oluşturuldu
+                return Ok();
             }
 
             else if ((responseJson["status"].ToString() == "failed") && (Convert.ToInt32(response.StatusCode) == 200))
             {
-                //hata döndür
+                return BadRequest();
             }
 
             else
             {
-                // hata döndür
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult External(TransactionModel transactionModel)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://207.154.196.92:5002/");
+
+            string jsonData = JsonConvert.SerializeObject(transactionModel);
+
+            var content = new StringContent(jsonData.ToString(), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = httpClient.PostAsync("/api/Transaction/external", content).Result;
+
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+
+            JObject responseJson = JsonConvert.DeserializeObject(responseBody) as JObject;
+
+
+            if ((responseJson["status"].ToString() == "success") && (Convert.ToInt32(response.StatusCode) == 200))
+            {
+                HttpContext.Session.SetString("token", responseJson["token"].ToString());
+                HttpContext.Session.SetString("tc", responseJson["tc"].ToString());
+                HttpContext.Session.SetString("firstName", responseJson["firstName"].ToString());
+                HttpContext.Session.SetString("lastName", responseJson["lastName"].ToString());
+                HttpContext.Session.SetString("phoneNumber", responseJson["phoneNumber"].ToString());
+                HttpContext.Session.SetString("no", responseJson["no"].ToString());
+                return RedirectToAction("Index", "Account");
+            }
+
+            else if ((responseJson["status"].ToString() == "failed") && (Convert.ToInt32(response.StatusCode) == 200))
+            {
+                return RedirectToAction("Register", "Auth");
+                //databag ile hatayı döndür register sayfasına ve sayfada göster
+            }
+
+            else
+            {
+                // register ekranına dön hata oluştu yazısı yazdır.
             }
             return View();
         }
+
+
     }
 }
